e6fb78eeab93aa399dbbb6c5ef7c9bede33206c2
szczekulskij <szczekulskij@gmail.com>
2023-12-04 13:23:47 -0800

    fix minor bugs for room and world

--- src/world.py
+++ src/world.py
@@ -1,4 +1,7 @@
-from warnings import warn
+import warnings
+from src import Room
+import itertools
+
 class World:
     """Core world modeling class."""
 
@@ -31,7 +34,6 @@ class World:
         self.rooms = []
         self.locations = []
         self.objects = []
-        self.set_metadata()
 
         # Counters
         self.num_rooms = 0
@@ -41,8 +43,8 @@ class World:
         self.object_instance_counts = {}
 
         # World bounds, will be set by update_bounds()
-        self.x_bounds = None
-        self.y_bounds = None
+        self.x_bounds = None # list of [xmin, xmax], automatically updated by update_bounds() whenever a room is added or removed
+        self.y_bounds = None # list of [ymin, ymax], automatically updated by update_bounds() whenever a room is added or removed
 
     def add_room(self, room_coordinates, room_color=None):
         r"""
@@ -55,29 +57,25 @@ class World:
 
         # If it's a room object, get it from the "room" named argument.
         # Else, create a room directly from the specified arguments.
-        room = room(room_coordinates, room_color=room_color)
+        room = Room(room_coordinates, room_color=room_color)
 
         # Check if the room collides with any other rooms or hallways
-        is_valid_pose = True
-        for other_loc in self.rooms + self.hallways:
-            if room.external_collision_polygon.intersects(
-                other_loc.external_collision_polygon
-            ):
-                is_valid_pose = False
-                break
-        if not is_valid_pose:
-            warnings.warn(f"Room {room.name} in collision. Cannot add to world.")
-            return None
+        # is_valid_pose = True
+        # for other_loc in self.rooms + self.hallways:
+        #     if room.external_collision_polygon.intersects(
+        #         other_loc.external_collision_polygon
+        #     ):
+        #         is_valid_pose = False
+        #         break
+        # if not is_valid_pose:
+        #     warnings.warn(f"Room {room.name} in collision. Cannot add to world.")
+        #     return None
 
         self.rooms.append(room)
         self.name_to_entity[room.name] = room
         self.num_rooms += 1
         self.update_bounds(entity=room)
 
-        # Update the room collision polygon based on the world inflation radius
-        room.update_collision_polygons(self.inflation_radius)
-
-        room.add_graph_nodes()
         return room
 
     def remove_room(self, room_name):
@@ -109,3 +107,49 @@ class World:
         self.update_bounds(entity=room, remove=True)
 
         return True
+    
+
+    def update_bounds(self, entity, remove=False):
+        """
+        Updates the X and Y bounds of the world.
+
+        :param entity: The entity that is being added or removed
+        :type entity: :class:`pyrobosim.core.room.Room`/:class:`pyrobosim.core.hallway.Hallway`
+        :param remove: Specifies if the update is due to removal of an entity.
+        :type remove: bool
+        """
+        if isinstance(entity, (Room)):
+            (xmin, ymin, xmax, ymax) = entity.polygon.bounds
+
+            if not self.x_bounds:
+                # When adding the first room
+                self.x_bounds = [xmin, xmax]
+                self.y_bounds = [ymin, ymax]
+                return
+
+            if remove:
+                sets_x_bounds = (self.x_bounds[0] == xmin) or (self.x_bounds[1] == xmax)
+                sets_y_bounds = (self.y_bounds[0] == ymin) or (self.y_bounds[1] == ymin)
+                is_last_room = len(self.rooms) == 0 and isinstance(entity, Room)
+                # Only update if the Room/Hallway being removed affects the bounds
+                if sets_x_bounds or sets_y_bounds:
+                    for other_entity in itertools.chain(self.rooms, self.hallways):
+                        (xmin, ymin, xmax, ymax) = other_entity.polygon.bounds
+                        self.x_bounds[0] = min(self.x_bounds[0], xmin)
+                        self.x_bounds[1] = max(self.x_bounds[1], xmax)
+                        self.y_bounds[0] = min(self.y_bounds[0], ymin)
+                        self.y_bounds[1] = max(self.y_bounds[1], ymax)
+                if is_last_room:
+                    # When last room has been deleted
+                    self.x_bounds = None
+                    self.y_bounds = None
+            else:
+                # Adding a Room/Hallway
+                self.x_bounds[0] = min(self.x_bounds[0], xmin)
+                self.x_bounds[1] = max(self.x_bounds[1], xmax)
+                self.y_bounds[0] = min(self.y_bounds[0], ymin)
+                self.y_bounds[1] = max(self.y_bounds[1], ymax)
+        else:
+            warnings.warn(
+                f"Updating bounds with unsupported entity type {type(entity)}"
+            )
