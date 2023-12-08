aeca6b72b57cf1e92a1739a7248b801b26ea0471
szczekulskij <szczekulskij@gmail.com>
2023-12-08 11:50:32 -0800

    finish demo update post random table generation code re-factor

--- demo.py
+++ demo.py
@@ -24,14 +24,9 @@ def create_world():
         room_name = "room1",
         room_color=[0, 0, 0],
     )
-    # r2coords = [(1.75, 2.5), (3.5, 2.5), (3.5, 4), (1.75, 4)]
-    # world.add_room(footprint=r2coords, color=[0, 0.6, 0])
-    # r3coords = [(-1, 1), (-1, 3.5), (-3.0, 3.5), (-2.5, 1)]
-    # world.add_room(footprint=r3coords, color=[0, 0, 0.6])
 
-    t1coords = [(-15, -5), (-15, 5), (-10, 5), (-10, -5) ]
-    table1 = world.add_table(
-        table_coordinates = t1coords,
+    table1 = world.add_random_table(
+        room_name = "room1",
         parent = "room1",
         name = "table1",
     )
